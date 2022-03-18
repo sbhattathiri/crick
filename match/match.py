1c24052f2779086bffc493f841f1044900a6fcdd
surajvbhattathiri <surajv.bhattathiri@nielsen.com>
2022-03-18 20:45:11 +0530

    removed unnecessary code

--- match/match.py
+++ match/match.py
@@ -6,15 +6,19 @@ import pandas as pd
 
 class Innings:
 
-    def __init__(self, team, target=None, overs=20):
-        self.team = team
-        self.opposition = 'team2' if team == 'team1' else 'team1'
+    def __init__(self, batting_team, target=None, max_overs=20):
+        self.batting_team = batting_team
+        self.bowling_team = 'team2' if self.batting_team == 'team1' else 'team1'
         self.target = target
-        self.overs = overs
-        self.score = 0
-        self.wickets = 0
+        self.max_overs = max_overs
+        self.total_runs = 0
+        self.total_wickets = 0
+        self.overs_completed = 0
         self.batting_card = {}
-        self.batters = self.get_batters(self.team)['name'].to_list()
+        self.batters = self.get_batters(self.batting_team)['name'].to_list()
+        self.bowling_card = {}
+        self.opposition_bowlers = self.get_bowlers(self.bowling_team)['name'].to_list()
+
     
     @staticmethod
     def isChadOrVirgin():
@@ -65,11 +69,6 @@ class Innings:
 
     def over(self, mode, runs_remaining, wickets_remaining):
     
-        # batters
-        striker =  self.batters[0]
-        non_striker = self.batters[1]
-        latest_batting_position_in_crease = 1 #incase of wickets
-
         # define possibilities for a ball bowled      
         possibilities = ['0', '1', '2', '3', '4', '6', 'W']
 
@@ -85,53 +84,35 @@ class Innings:
         wickets = 0
         balls_bowled = 0
 
-        striker_runs = 0
-        striker_balls = 0
-        non_striker_runs = 0
-        non_striker_balls = 0
-
         for _ in range(6):
-            striker_stats = {}
-            non_striker_stats = {}
             event = random.choices(possibilities, weights=weights, k=1)[0]
             balls_bowled += 1
             if event == 'W':
                 wickets += 1
-                striker_stats['runs'] = striker_stats.get('runs', 0)
-                striker_stats['balls'] = striker_stats.get('balls', 0) + 1
-                # batting_card[striker] = striker_stats
             else:
                 runs += int(event)
 
             # end innings if all wickets lost
             # or target achieved    
             if wickets >= wickets_remaining or runs >= runs_remaining:
-                print(f'Innings over')
                 break
 
-        print(f'{runs}, {wickets}, {balls_bowled/10}')
         return runs, wickets, balls_bowled
 
     
     def play(self):
+        # init previous bowler so than end's change
+        previous_bowler = ''
+
         # assign target to chase or 20*36 if first inn
         target = self.target if self.target else 720
-        
-        total_runs = 0
-        total_wickets = 0
-        overs_completed = 0
-
-        bowling_card = {}
-        opposition_bowlers_df = self.get_bowlers(self.opposition)
-        opposition_bowlers = opposition_bowlers_df['name'].to_list()
-
-        previous_bowler = ''
-        while overs_completed < 20 and total_wickets < 10 and total_runs <= target: 
+    
+        while self.overs_completed < 20 and self.total_wickets < 10 and self.total_runs <= target: 
             # choose a bowler
             while True:
-                bowler = random.choice(opposition_bowlers)
+                bowler = random.choice(self.opposition_bowlers)
                 if bowler != previous_bowler:
-                    bowler_stats = bowling_card.get(bowler, {})
+                    bowler_stats = self.bowling_card.get(bowler, {})
                     if bowler_stats.get('overs', 0) < 4:
                         break
                         
@@ -145,14 +126,14 @@ class Innings:
                 bowlers_form = 'normal'
 
             # bowl the over
-            runs, wickets, balls_bowled = self.over(mode, target-total_runs, 10-total_wickets)
+            runs, wickets, balls_bowled = self.over(mode, target-self.total_runs, 10-self.total_wickets)
 
-            print(f'Over: {overs_completed + 1}, Bowler: {bowler}, {bowlers_form}, {runs}-{wickets}')
+            print(f'Over: {self.overs_completed + 1}, Bowler: {bowler}, {bowlers_form}, {runs}-{wickets}')
             
             # scoring
-            total_runs += runs
-            total_wickets += wickets
-            overs_completed = overs_completed + 1 if balls_bowled == 6 else overs_completed + (balls_bowled / 10)
+            self.total_runs += runs
+            self.total_wickets += wickets
+            self.overs_completed = self.overs_completed + 1 if balls_bowled == 6 else self.overs_completed + (balls_bowled / 10)
             
             # build bowler card
             bowler_overs = bowler_stats.get('overs', 0) + 1 if balls_bowled == 6 else bowler_stats.get('overs', 0) + (balls_bowled / 10)
@@ -162,20 +143,19 @@ class Innings:
             bowler_stats['runs'] = bowler_runs
             bowler_stats['wickets'] = bowler_wickets
 
-            bowling_card[bowler] = bowler_stats
+            self.bowling_card[bowler] = bowler_stats
 
+            # set current bowler as previous
             previous_bowler = bowler
-        
-        return bowling_card
 
     
-    def display_bowling_card(self, bowling_card):
-        for bowler in bowling_card:    
-            print(f'{bowler} {bowling_card[bowler]["overs"]} - {bowling_card[bowler]["runs"]} - {bowling_card[bowler]["wickets"]}')
+    def display_bowling_card(self):
+        for bowler in self.bowling_card:    
+            print(f'{bowler} \t {self.bowling_card[bowler]["overs"]} - {self.bowling_card[bowler]["runs"]} - {self.bowling_card[bowler]["wickets"]}')
 
 
             
 if __name__ == '__main__':
     innings = Innings('team1')
-    bowling_card = innings.play()
-    innings.display_bowling_card(bowling_card=bowling_card)
+    innings.play()
+    innings.display_bowling_card()
